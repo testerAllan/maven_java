613e295d1bc3c0bfccd57e71114132cb05ee56f2
AlanRaak <56818442+AlanRaak@users.noreply.github.com>
2020-04-06 21:38:21 +0300

    Valmis

--- Kodutöö/src/test/java/hello/CounterTest.java
+++ Kodutöö/src/test/java/hello/CounterTest.java
@@ -3,30 +3,34 @@ package hello;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
  
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
- 
-import com.howtodoinjava.demo.mockito.DatabaseDAO;
-import com.howtodoinjava.demo.mockito.NetworkDAO;
-import com.howtodoinjava.demo.mockito.RecordService;
- 
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 @RunWith(MockitoJUnitRunner.class)
-public class ApplicationTest 
+public class CounterTest 
 {
-    //@InjectMocks
+
+    //Counter counter = new Counter(); // Välja kommenteeritud on test, kus ei kasuta mocki
     @Mock
-    Counter counterMock;
+    Counter counterMock; //Küsimus 3: Kas siin on mocki tegemine vajalik? Counter ei kasuta ühtegi teist klassi ju.
      
     @Test
     public void saveTest()
     {
-        String saved = counterMock.nameLength("Jaanus");
-        assertEquals("Tere", saved);
-         
-        verify(counterMock.nameLength, times(1)).save("temp.txt"); // hows
+
+        int saved = counterMock.nameLength("Jaanus");// Küsimus 7: Miks "saved" väärtus tuleb 0, kas counterMock ei kasuta mitte mingit loogikat Counter klassist?
+        assertEquals(0, saved); 
+        //int saved = counter.nameLength("Jaanus");
+        //assertEquals(6, saved);
     }
 }
--- Kodutöö/src/test/java/hello/GreeterTest.java
+++ Kodutöö/src/test/java/hello/GreeterTest.java
@@ -1,17 +1,35 @@
 package hello;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+ 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class GreeterTest {
+@RunWith(MockitoJUnitRunner.class) // Küsimus 4: Kas sellel on mingi põhjus siin või see lihtsalt peab alati olema, kui kasutan Mockitot?
+public class GreeterTest 
+{
+    @InjectMocks // Küsimus 1: Kas see siin tekitab Greeter objekti, mis edaspidi kasutab Counter klassi "mocki"(koopiat) ?
+    Greeter greeterMock; // Küsimus 1: Ehk InjectMocks loob objetci, mis automaatselt hakkab kasutama edaspidi loodud mocke, mida Greeter klass funksioon sayHello() kasutab.
 
-    private Greeter greeter = new Greeter();
-
+    @Mock // Küsimus 2: Kas see Mock on vajalik? Test kasutab ainult greeterMock objecti ju.
+    Counter counterMock;
+     
     @Test
-    public void greeterSaysHello() {
-        assertThat(greeter.sayHello("Jaanus"), containsString("Tere"));
-    }
+    public void saveTest()
+    {
+        // when(counterMock.nameLength("Jaanus")).thenReturn(6); // Küsimus 5: Seda peaksin kasutama siis, kui näiteks Counter suhtleb andmebaasiga ja testis ma ei taha, et ta seda teeks?
+        assertThat(greeterMock.sayHello("Jaanus"), containsString("Tere")); // Küsimus 6: Kas see osa siin nüüd ei kasuta või ei tekita Counter klassi objekti greeterMock.sayHello("Jaanus") sees?
 
+    }
 }
